fd65443f394289620416d7d8fcf1a21f6cda7699
harma <w0833331@myscc.ca>
2025-10-01 13:57:51 -0400

    make two components using ng command

---
+++ src/app/game-list-item/game-list-item.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-list-item',
+  imports: [],
+  templateUrl: './game-list-item.html',
+  styleUrl: './game-list-item.css'
+})
+export class GameListItem {
+
+}
