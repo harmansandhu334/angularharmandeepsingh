03afb5e64202e2879f5640326affb68c78d0bcd0
harma <w0833331@myscc.ca>
2025-10-10 09:31:39 -0400

    completed 8 and 9step

--- src/app/services/game.ts
+++ src/app/services/game.ts
@@ -8,13 +8,44 @@ import { mockContent } from '../data/mockcontent';
   providedIn: 'root'
 })
 export class Game {
-  private games: User[] = mockContent; // Local copy of game data for CRUD Operations
+  private games: User[] = mockContent; // Local copy of game data
 
   constructor() { }
 
   // Returns all games
   getGames(): Observable<User[]> {
-    return of(mockContent); // Return an observable that emits mock game data
+    return of(mockContent); // Returns an observable that emits mock game data
+
+
   }
+  //read
+  //getting one game with id
+  getGameWithId(id: number): Observable<User | undefined> {
+    return of(this.games.find(g => g.id === id));
+  }
+
+  // adding new game
+  addGame(newGame: User): Observable<User[]> {
+    this.games.push(newGame);
+    return of(this.games);
+  }
+
+
+  //updating game
+  updateGame(updatedGame: User): Observable<User[]> {
+    const index = this.games.findIndex(g => g.id === updatedGame.id);
+    if (index !== -1) this.games[index] = updatedGame;
+    return of(this.games);
+  }
+
+  //  deleting game
+  deleteGame(id: number): Observable<User | undefined> {
+    const index = this.games.findIndex(g => g.id === id);
+    if (index === -1) return of(undefined);
+    const [removed] = this.games.splice(index, 1);
+    return of(removed);
+  }
+
+
 
 }
