ec8398c32165acd8e45e3c66867a0fd82084c47f
harma <w0833331@myscc.ca>
2025-10-09 22:49:19 -0400

    completed 6 and 7step

--- src/app/game-list/game-list.ts
+++ src/app/game-list/game-list.ts
@@ -2,15 +2,24 @@ import { Component } from '@angular/core';
 import { User } from '../models/user';
 import { CommonModule } from '@angular/common';
 import { GameListItem } from '../game-list-item/game-list-item';
+import { Game } from '../services/game';
 
 @Component({
   selector: 'app-game-list',
   imports: [CommonModule, GameListItem],
   templateUrl: './game-list.html',
   standalone: true,
-  styleUrl: './game-list.css'
+  styleUrls: ['./game-list.css']
 })
 export class GameList {
   gamesList: User[] = [];
+  // dependency injection
+  constructor(private game: Game) {}
 
-}
+
+  ngOnInit(): void {
+    this.game.getGames().subscribe(list => {
+      this.gamesList = list;
+    });
+
+}}
--- src/app/services/game.ts
+++ src/app/services/game.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { User } from '../models/user';
+import { mockContent } from '../data/mockcontent';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class Game {
+  private games: User[] = mockContent; // Local copy of game data for CRUD Operations
+
+  constructor() { }
+
+  // Returns all games
+  getGames(): Observable<User[]> {
+    return of(mockContent); // Return an observable that emits mock game data
+  }
 
 }
