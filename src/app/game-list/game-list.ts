fb8eaa13f82465b0da0740f83e34527d3f344eb0
harma <w0833331@myscc.ca>
2025-10-09 21:55:53 -0400

    completed 4 and 5 step

--- src/app/game-list/game-list.ts
+++ src/app/game-list/game-list.ts
@@ -11,12 +11,6 @@ import { GameListItem } from '../game-list-item/game-list-item';
   styleUrl: './game-list.css'
 })
 export class GameList {
-  gamesList: User[] = [
-    { id: 1, title: 'Cricket 24',     genre: 'Sports',      inStock: true,  multiplayer: true  },
-    { id: 2, title: 'Ashes Cricket',  genre: 'Sports',      inStock: false, multiplayer: true  },
-    { id: 3, title: 'Kabaddi Clash',  genre: 'Sports',      inStock: true,  multiplayer: true  },
-    { id: 4, title: 'Carrom Board',   genre: 'Indoor',      inStock: true,  multiplayer: true  }
-
-  ];
+  gamesList: User[] = [];
 
 }
