b1b1463d58521a2184d5db7023f29121d8f3519e
harma <w0833331@myscc.ca>
2025-10-16 21:39:42 -0400

    step 4 and 5

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { provideRouter, Routes } from '@angular/router';
+import { GameList } from './app/game-list/game-list';
+import { GameListItem } from './app/game-list-item/game-list-item';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+
+
+// routes
+const routes: Routes = [
+
+  { path: 'games', component: GameList },
+  { path: 'game-item', component: GameListItem }
+
+];
+bootstrapApplication(App, {
+  providers: [provideRouter(routes)]
+})
+  .then(r => console.log('Bootstrap successful'));
